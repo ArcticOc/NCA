7ba207657a15c6443431c7769cbbb4df108fe37c
koko <koko>
2023-12-12 02:12:58 +0900

    DDP

--- src/configs/get_configs.py
+++ src/configs/get_configs.py
@@ -1,7 +1,10 @@
-import torch
 import pickle
-from torch.optim.lr_scheduler import MultiStepLR, StepLR, CosineAnnealingLR
+
+import torch
 import torch.nn.functional as F
+from torch.optim.lr_scheduler import CosineAnnealingLR, MultiStepLR, StepLR
+from torch.utils.data.distributed import DistributedSampler as Sampler
+
 import src.datasets as datasets
 
 
@@ -21,36 +24,42 @@ def get_dataloader(split, args, aug=False, shuffle=True, out_name=False, sample=
     # sample: iter, way, shot, query
     if aug:
         transform = datasets.with_augment(
-            84, disable_random_resize=args.disable_random_resize)
+            84, disable_random_resize=args.disable_random_resize
+        )
     else:
         transform = datasets.without_augment(84, enlarge=args.enlarge)
 
     sets = datasets.DatasetFolder(
-        args.data, args.split_dir, split, transform, out_name=out_name)
+        args.data, args.split_dir, split, transform, out_name=out_name
+    )
+    DDP_sampler = Sampler(sets)
     if sample is not None:
         # this is for prototypical network training
-        sampler = datasets.CategoriesSampler(sets.labels,
-                                             args.episode_no_replacement_sampling,
-                                             *sample)
+        sampler = datasets.CategoriesSampler(
+            sets.labels, args.episode_no_replacement_sampling, *sample
+        )
 
-        loader = torch.utils.data.DataLoader(sets, batch_sampler=sampler,
-                                             num_workers=args.workers, pin_memory=True)
+        loader = torch.utils.data.DataLoader(
+            sets, batch_sampler=sampler, num_workers=args.workers, pin_memory=True
+        )
     else:
         # this is for normal sampling
         if args.replacement_sampling:
-            sampler = datasets.ReplacementSampler(sets.labels, args.proto_train_iter,
-                                                  args.batch_size)
+            sampler = datasets.ReplacementSampler(
+                sets.labels, args.proto_train_iter, args.batch_size
+            )
 
-            loader = torch.utils.data.DataLoader(sets,
-                                                 batch_sampler=sampler,
-                                                 num_workers=args.workers,
-                                                 pin_memory=True)
+            loader = torch.utils.data.DataLoader(
+                sets, batch_sampler=sampler, num_workers=args.workers, pin_memory=True
+            )
         else:
-            loader = torch.utils.data.DataLoader(sets,
-                                                 batch_size=args.batch_size,
-                                                 shuffle=shuffle,
-                                                 num_workers=args.workers,
-                                                 pin_memory=True)
+            loader = torch.utils.data.DataLoader(
+                sets,
+                batch_size=args.batch_size,
+                sampler=DDP_sampler,
+                num_workers=args.workers,
+                pin_memory=True,
+            )
     return loader
 
 
@@ -61,10 +70,17 @@ def get_metric(metric_type, args):
     :return metric function (callable function):
     """
     METRICS = {
-        'cosine': lambda gallery, query: 1. - F.cosine_similarity(query[:, None, :], gallery[None, :, :], dim=2),
-        'euclidean': lambda gallery, query: ((query[:, None, :] - gallery[None, :, :]) ** 2).sum(2),
-        'l1': lambda gallery, query: torch.norm((query[:, None, :] - gallery[None, :, :]), p=1, dim=2),
-        'l2': lambda gallery, query: torch.norm((query[:, None, :] - gallery[None, :, :]), p=2, dim=2),
+        "cosine": lambda gallery, query: 1.0
+        - F.cosine_similarity(query[:, None, :], gallery[None, :, :], dim=2),
+        "euclidean": lambda gallery, query: (
+            (query[:, None, :] - gallery[None, :, :]) ** 2
+        ).sum(2),
+        "l1": lambda gallery, query: torch.norm(
+            (query[:, None, :] - gallery[None, :, :]), p=1, dim=2
+        ),
+        "l2": lambda gallery, query: torch.norm(
+            (query[:, None, :] - gallery[None, :, :]), p=2, dim=2
+        ),
     }
     return METRICS[metric_type]
 
@@ -76,26 +92,39 @@ def get_scheduler(batches, optimizer, args):
     :return: scheduler
     """
 
-    SCHEDULER = {'step': StepLR(optimizer, args.lr_stepsize, args.lr_gamma),
-                 'multi_step': MultiStepLR(optimizer,
-                                           milestones=[round(float(x)*args.epochs) for x in args.lr_milestones.split(',')],
-                                           gamma=args.lr_gamma),
-                 'cosine': CosineAnnealingLR(optimizer, batches * args.epochs, eta_min=1e-9)}
+    SCHEDULER = {
+        "step": StepLR(optimizer, args.lr_stepsize, args.lr_gamma),
+        "multi_step": MultiStepLR(
+            optimizer,
+            milestones=[
+                round(float(x) * args.epochs) for x in args.lr_milestones.split(",")
+            ],
+            gamma=args.lr_gamma,
+        ),
+        "cosine": CosineAnnealingLR(optimizer, batches * args.epochs, eta_min=1e-9),
+    }
     return SCHEDULER[args.scheduler]
 
 
 def save_pickle(file, data):
-    with open(file, 'wb') as f:
+    with open(file, "wb") as f:
         pickle.dump(data, f)
 
 
 def load_pickle(file):
-    with open(file, 'rb') as f:
+    with open(file, "rb") as f:
         return pickle.load(f)
 
 
 def get_optimizer(module, args):
-    OPTIMIZER = {'SGD': torch.optim.SGD(module.parameters(), lr=args.lr, momentum=0.9, weight_decay=args.weight_decay,
-                                        nesterov=args.nesterov),
-                 'Adam': torch.optim.Adam(module.parameters(), lr=args.lr)}
+    OPTIMIZER = {
+        "SGD": torch.optim.SGD(
+            module.parameters(),
+            lr=args.lr,
+            momentum=0.9,
+            weight_decay=args.weight_decay,
+            nesterov=args.nesterov,
+        ),
+        "Adam": torch.optim.Adam(module.parameters(), lr=args.lr),
+    }
     return OPTIMIZER[args.optimizer]
